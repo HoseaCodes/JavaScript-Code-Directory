8ddbfd1a3423aa2b7eeba9896bfcd6a648207dd8
hoseacodes <dominique11h@yahoo.com>
2021-01-24 17:24:07 -0600

    Updated Layout

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,8 @@
 const kanye = document.getElementById("kanye")
 const life = document.getElementById("life")
-const loadItems = [kanye, life]
+const bmi = document.getElementById("BMI")
+const bandGenerator = document.getElementById("BandGenerator")
+const loadItems = [kanye, life, bmi, bandGenerator]
 
 for (let index = 0; index < loadItems.length; index++) {
     const element = loadItems[index];
